d592cb20b908fcd013c82ea412f38e2193f0beab
Batuka0213 <batsaihanbatnasan123@gmail.com>
2026-05-04 18:12:02 +0800

    fix: add basename /Diplom for GitHub Pages routing

--- lostfound-frontend/src/App.js
+++ lostfound-frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}>
-    <Router>
+    <Router basename="/Diplom">
       <TopBar />
       <Toaster
         position="top-right"
